4116e45f4b9c026c6a362a0708e313882598c653
Khamisali61 <khamisali61@gmail.com>
2017-02-09 12:09:09 +0300

    Create Binary_Search.py

---
+++ Binary_Search.py
@@ -0,0 +1,3 @@
+class BinarySearch(n): #Creates class binarysearch which takes in list n as an argument
+  def __init__(self, a, b): #Creates a list of length a with steps b between list elements
+  
